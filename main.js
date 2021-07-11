364c374263ee12c6946f11c83d52c3f34feabd3b
Krysia Masińska <krysiamasinska@gmail.com>
2021-07-11 20:29:34 +0200

    Changed name of functions

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ const errorP = document.querySelector('.error');
 const infoCost = document.querySelector('.cost_info');
 const totalCost = document.querySelector('.cost_total');
 
-const checkInputValue = () => {
+const checkBillTip = () => {
     errorP.textContent = '';
 
     if (inputPrice.value > 0 && inputPeople.value > 0 && billTip.value > 0) {
         infoCost.style.display = 'block';
-        totalCost.textContent = countTip();
+        totalCost.textContent = countBillTip();
     } else {
         infoCost.style.display = 'none';
         errorP.textContent = 'Wypełnij wszystkie pola';
     }
 };
 
-const countTip = () => {
+const countBillTip = () => {
     const price = parseFloat(inputPrice.value);
     const people = parseFloat(inputPeople.value);
     const billTips = parseFloat(billTip.value);
@@ -26,4 +26,4 @@ const countTip = () => {
     return result.toFixed(2);
 }
 
-btnCount.addEventListener('click', checkInputValue);
+btnCount.addEventListener('click', checkBillTip);
